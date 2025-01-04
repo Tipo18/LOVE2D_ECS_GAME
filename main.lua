1fda7037d9f1deb1903bfd4db5760e117d354453
Tipo18 <tim.darma@hotmail.fr>
2025-01-04 12:45:41 +0100

    start menue

--- main.lua
+++ main.lua
@@ -7,6 +7,10 @@ local love = require "love"
 
 -- Lua est dynamiquement typé et ses tableaux sont flexibles
 
+local world = {
+    gamestate = "menu" -- menu / running / paused
+}
+
 -- index / joueur / display / xpos / ypos / xsiz / ysiz
 local entities = {}
 
@@ -47,22 +51,41 @@ local function deplacementSystem(entities)
     end
 end
 
-function love.load()
-    love.window.setTitle("LOVE2D GAME")
-    love.window.setMode(1200, 900, { resizable = false })
+local function gamestart()
+    world.gamestate = "running"
     table.insert(entities, { 1, true, true, true, true, true, true })
     table.insert(composants, { 50, 40, 50, 100 })
     table.insert(entities, { 2, false, true, true, true, true, true })
     table.insert(composants, { 200, 100, 50, 100 })
 end
 
+function love.load()
+    love.window.setTitle("LOVE2D GAME")
+    love.window.setMode(1200, 900, { resizable = false })
+    -- gamestart()
+end
+
 function love.update()
-    deplacementSystem(entities)
+    if world.gamestate == "menu" then
+        if love.keyboard.isDown("return") then
+            gamestart()
+        end
+    end
+    if world.gamestate == "running" then
+        deplacementSystem(entities)
+    end
     print("update")
 end
 
-
-
 function love.draw()
-    renderSystem(entities)
+    if world.gamestate == "menu" then
+        love.graphics.setNewFont(30)
+        local text = "Press Enter to start"
+        local width = love.graphics.getWidth()
+        local height = love.graphics.getHeight()
+        love.graphics.print(text, width/2 - love.graphics.getFont():getWidth(text)/2, height*(3/5))
+    end
+    if world.gamestate == "running" then
+        renderSystem(entities)
+    end
 end
