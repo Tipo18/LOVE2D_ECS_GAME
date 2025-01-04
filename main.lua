b6254204ac06aa721113d0bca84b35681768c99c
Tipo18 <tim.darma@hotmail.fr>
2025-01-04 12:21:43 +0100

    multiples entities

--- main.lua
+++ main.lua
@@ -7,9 +7,10 @@ local love = require "love"
 
 -- Lua est dynamiquement typé et ses tableaux sont flexibles
 
--- index / joueur / display / xpos / ypos
+-- index / joueur / display / xpos / ypos / xsiz / ysiz
 local entities = {}
 
+-- xpos / ypos / xsiz / ysiz
 local composants = {}
 
 local systems = {
@@ -19,7 +20,9 @@ local function renderSystem(entities)
     for _, entity in ipairs(entities) do
         if entity[3] then
             local index = entity[1]
-            love.graphics.rectangle("fill", composants[index][3], composants[index][4], 100, 100)
+            print(index)
+            love.graphics.rectangle("fill", composants[index][1], composants[index][2], composants[index][3],
+                composants[index][4])
         end
     end
 end
@@ -27,8 +30,10 @@ end
 function love.load()
     love.window.setTitle("LOVE2D GAME")
     love.window.setMode(1200, 900, { resizable = false })
-    table.insert(entities, { 1, true, true, true, true })
-    table.insert(composants, { true, true, 50, 40 })
+    table.insert(entities, { 1, true, true, true, true, true, true })
+    table.insert(composants, { 50, 40, 50, 100 })
+    table.insert(entities, { 2, true, true, true, true, true, true })
+    table.insert(composants, { 200, 100, 50, 100 })
 end
 
 function love.update()
