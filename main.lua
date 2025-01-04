a00bfbe1fb2f147321973eeaf1a1b051ee58b89b
Tipo18 <tim.darma@hotmail.fr>
2025-01-04 12:16:37 +0100

    basic ecs rendering s wk

--- main.lua
+++ main.lua
@@ -5,9 +5,30 @@ local love = require "love"
 
 -- love.load -> love.update -> love.draw
 
+-- Lua est dynamiquement typé et ses tableaux sont flexibles
+
+-- index / joueur / display / xpos / ypos
+local entities = {}
+
+local composants = {}
+
+local systems = {
+}
+
+local function renderSystem(entities)
+    for _, entity in ipairs(entities) do
+        if entity[3] then
+            local index = entity[1]
+            love.graphics.rectangle("fill", composants[index][3], composants[index][4], 100, 100)
+        end
+    end
+end
+
 function love.load()
     love.window.setTitle("LOVE2D GAME")
     love.window.setMode(1200, 900, { resizable = false })
+    table.insert(entities, { 1, true, true, true, true })
+    table.insert(composants, { true, true, 50, 40 })
 end
 
 function love.update()
@@ -17,5 +38,5 @@ function love.update()
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", 100, 100, 50, 50)
+    renderSystem(entities)
 end
