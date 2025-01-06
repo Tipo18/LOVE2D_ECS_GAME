c3fe524d40436b446f817ce4c02485048b408650
Tipo18 <tim.darma@hotmail.fr>
2025-01-06 11:35:52 +0100

    platform mec and coyote timer

--- main.lua
+++ main.lua
@@ -45,29 +45,53 @@ local function deplacementSystem(dt)
     if love.keyboard.isDown("left") then
         components[1].xvelocity = -300
     end
-    function love.keypressed(key)
-        if key == "space" and entities[1].onground then
-            print(entities[1].onground)
-            components[1].yvelocity = -500
+    if love.keyboard.isDown("space") then
+        if entities[1].onground or components[1].coyotetimer <= 0.1 then
+            print(components[1].coyotetimer)
+            components[1].yvelocity = -700
             entities[1].onground = false
+            components[1].coyotetimer = components[1].coyotetimer + 1
         end
     end
 
+    -- switch pour un fonctionnement avec platforme
+    local verif_on_no_plat = 0
+    for _, entity in ipairs(entities) do
+        if entity.platforme then
+            if components[1].xpos < components[entity.index].xpos + components[entity.index].xsize and
+                components[1].xpos + components[1].xsize > components[entity.index].xpos and
+                components[1].ypos < components[entity.index].ypos + components[entity.index].ysize and
+                components[1].ypos + components[1].ysize > components[entity.index].ypos then
+                if components[1].ypos + components[1].ysize <= components[entity.index].ypos + 10 then
+                    print(entity.index)
+                    entities[1].onground = true
+                    components[1].yvelocity = 0
+                    components[1].ypos = components[entity.index].ypos - components[1].ysize
+                    verif_on_no_plat = verif_on_no_plat + 1
+                end
+            end
+        end
+    end
+    if verif_on_no_plat == 0 then
+        entities[1].onground = false
+    end
+
     if entities[1].onground == false then
-        components[1].yvelocity = components[1].yvelocity + 750 * dt
+        components[1].yvelocity = components[1].yvelocity + 1000 * dt
+        components[1].coyotetimer = components[1].coyotetimer + dt
+    else
+        components[1].yvelocity = 0
+        components[1].coyotetimer = 0
     end
 
     -- movement
     components[1].xpos = components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5)
     components[1].ypos = components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5)
     components[1].xvelocity = 0
-
-    if components[1].ypos + components[1].ysize >= 1000 then
-        entities[1].onground = true
-        components[1].yvelocity = 0
-        components[1].ypos = 1000 - components[1].ysize
-    end
 end
+
+
+
 
 
 -- local function approxCheckCollision(indexe1, indexe2)
@@ -131,9 +155,9 @@ local function gamestart()
     local it_index = 1
     -- joueur
     table.insert(entities,
-        { index = it_index, player = true, spawn = false, end_door = false, platforme = false, wall = false, display = true, xpos = true, ypos = true, xvelocity = true, yvelocity = true, xsize = true, ysize = true, onground = true, })
+        { index = it_index, player = true, spawn = false, end_door = false, platforme = false, wall = false, display = true, xpos = true, ypos = true, xvelocity = true, yvelocity = true, xsize = true, ysize = true, onground = true, coyotetimer = true, })
     table.insert(components,
-        { xpos = 20, ypos = 1000 - 40, xsize = 20, ysize = 40, xvelocity = 250, yvelocity = 0, isonground = true })
+        { xpos = 20, ypos = 1000 - 40, xsize = 20, ysize = 40, xvelocity = 250, yvelocity = 0, isonground = true, coyotetimer = 0 })
     it_index = it_index + 1
     -- spawn
     table.insert(entities,
@@ -145,6 +169,27 @@ local function gamestart()
         { index = it_index, player = false, spawn = false, end_door = true, platforme = false, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 800, ypos = 1000 - 50, xsize = 30, ysize = 50 })
     it_index = it_index + 1
+
+    -- sol
+    table.insert(entities,
+        { index = it_index, player = false, spawn = false, end_door = true, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+    table.insert(components, { xpos = 0, ypos = 999, xsize = 1000, ysize = 10 })
+    it_index = it_index + 1
+
+    -- platforme
+    table.insert(entities,
+        { index = it_index, player = false, spawn = false, end_door = true, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+    table.insert(components, { xpos = 200, ypos = 900, xsize = 80, ysize = 20 })
+    it_index = it_index + 1
+    table.insert(entities,
+        { index = it_index, player = false, spawn = false, end_door = true, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+    table.insert(components, { xpos = 300, ypos = 800, xsize = 80, ysize = 20 })
+    it_index = it_index + 1
+    table.insert(entities,
+        { index = it_index, player = false, spawn = false, end_door = true, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+    table.insert(components, { xpos = 400, ypos = 700, xsize = 80, ysize = 20 })
+    it_index = it_index + 1
+    -- switching the game state
     world.gamestate = "running"
 end
 
