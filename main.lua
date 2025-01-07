ea915908e89529599d6e717e3082f871316e5c1e
Tipo18 <tim.darma@hotmail.fr>
2025-01-08 00:42:44 +0100

    no interference between colision and platforme tcheck

--- main.lua
+++ main.lua
@@ -71,111 +71,27 @@ local function inputSystem()
     end
 end
 
--- S de colission complet
-local function collisionPlatformSystem(dt)
-    local block_move = false
-    for _, entity in ipairs(entities) do
-        if entity.wall then
-            if components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5) < components[entity.index].xpos + components[entity.index].xsize and
-                components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5) + components[1].xsize > components[entity.index].xpos and
-                components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5) < components[entity.index].ypos + components[entity.index].ysize and
-                components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5) + components[1].ysize > components[entity.index].ypos then
-                block_move = true
-                -- Calculate the overlap (penetration depth) on the X axis
-                local overlapX1 = (components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5) + components[1].xsize) -
-                    components[entity.index].xpos
-                local overlapX2 = (components[entity.index].xpos + components[entity.index].xsize) -
-                    (components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5))
-
-                -- Take the smaller positive overlap value (to resolve in the correct direction)
-                local penetrationX = math.min(overlapX1, overlapX2)
-
-                -- Calculate the overlap (penetration depth) on the Y axis
-                local overlapY1 = (components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5) + components[1].ysize) -
-                    components[entity.index].ypos
-                local overlapY2 = (components[entity.index].ypos + components[entity.index].ysize) -
-                    (components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5))
-
-                -- Take the smaller positive overlap value (to resolve in the correct direction)
-                local penetrationY = math.min(overlapY1, overlapY2)
-                if penetrationX < penetrationY then
-                    -- Resolve X-axis collision
-                    components[1].xvelocity = 0                                                  -- Stop horizontal movement
-                    if overlapX1 < overlapX2 then
-                        components[1].xpos = components[entity.index].xpos - components[1].xsize -- Push left
-                    else
-                        components[1].xpos = components[entity.index].xpos + components[entity.index]
-                            .xsize -- Push right
-                    end
-                else
-                    components[1].yvelocity = 0 -- Stop vertical movement
-                    -- Resolve Y-axis collision
-                    if overlapY1 < overlapY2 then
-                        components[1].ypos = components[entity.index].ypos - components[1].ysize            -- Push up
-                    else
-                        components[1].ypos = components[entity.index].ypos + components[entity.index].ysize -- Push down
-                    end
-                end
-            end
-        end
-    end
-end
-
-
-local function platformeCheckSystem()
-    local verif_on_no_plat = 0
-    for _, entity in ipairs(entities) do
-        if entity.platforme then
-            if components[1].xpos < components[entity.index].xpos + components[entity.index].xsize and
-                components[1].xpos + components[1].xsize > components[entity.index].xpos and
-                components[1].ypos < components[entity.index].ypos + components[entity.index].ysize and
-                components[1].ypos + components[1].ysize > components[entity.index].ypos then
-                if components[1].ypos + components[1].ysize <= components[entity.index].ypos + 10 then
-                    entities[1].onground = true
-                    components[1].yvelocity = 0
-                    components[1].ypos = components[entity.index].ypos - components[1].ysize
-                    verif_on_no_plat = entity.index
-                end
-            end
-        end
-    end
-    if verif_on_no_plat == 0 then
-        entities[1].onground = false
-    end
-end
-
-local function fallingSystem(dt)
-    if entities[1].onground == false then
-        components[1].yvelocity = components[1].yvelocity + 1000 * dt
-        components[1].yvelocity = math.min(components[1].yvelocity, 700)
-        components[1].coyotetimer = components[1].coyotetimer + dt
-    else
-        components[1].yvelocity = 0
-        components[1].coyotetimer = 0
-    end
-end
-
-local function deplacementSystem(dt)
+local function collisionPlatformDeplacementSysteme(dt)
     local block_xmove = false
     local block_ymove = false
+
+    local next_xpos = components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5)
+    local next_ypos = components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5)
+
     for _, entity in ipairs(entities) do
         if entity.wall then
-            if components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5) < components[entity.index].xpos + components[entity.index].xsize and
-                components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5) + components[1].xsize > components[entity.index].xpos and
-                components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5) < components[entity.index].ypos + components[entity.index].ysize and
-                components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5) + components[1].ysize > components[entity.index].ypos then
-                local overlapX1 = (components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5) + components[1].xsize) -
-                    components[entity.index].xpos
-                local overlapX2 = (components[entity.index].xpos + components[entity.index].xsize) -
-                    (components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5))
+            if next_xpos < components[entity.index].xpos + components[entity.index].xsize and
+                next_xpos + components[1].xsize > components[entity.index].xpos and
+                next_ypos < components[entity.index].ypos + components[entity.index].ysize and
+                next_ypos + components[1].ysize > components[entity.index].ypos then
+                local overlapX1 = (next_xpos + components[1].xsize) - components[entity.index].xpos
+                local overlapX2 = (components[entity.index].xpos + components[entity.index].xsize) - next_xpos
                 local penetrationX = math.min(overlapX1, overlapX2)
 
-                local overlapY1 = (components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5) + components[1].ysize) -
-                    components[entity.index].ypos
-                local overlapY2 = (components[entity.index].ypos + components[entity.index].ysize) -
-                    (components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5))
-
+                local overlapY1 = (next_ypos + components[1].ysize) - components[entity.index].ypos
+                local overlapY2 = (components[entity.index].ypos + components[entity.index].ysize) - next_ypos
                 local penetrationY = math.min(overlapY1, overlapY2)
+
                 if penetrationX < penetrationY then
                     components[1].xvelocity = 0
                     block_xmove = true
@@ -197,18 +113,43 @@ local function deplacementSystem(dt)
         end
     end
 
-    if components[1].xvelocity ~= 0 then
-        walking = true
+    walking = components[1].xvelocity ~= 0
+
+    local verif_plat = 0
+    if not block_xmove and not block_ymove then
+        for _, entity in ipairs(entities) do
+            if entity.platforme then
+                if next_xpos < components[entity.index].xpos + components[entity.index].xsize and
+                    next_xpos + components[1].xsize > components[entity.index].xpos and
+                    next_ypos < components[entity.index].ypos + components[entity.index].ysize and
+                    next_ypos + components[1].ysize >= components[entity.index].ypos then
+                    entities[1].onground = true
+                    components[1].yvelocity = 0
+                    block_ymove = true
+                    components[1].ypos = components[entity.index].ypos - components[1].ysize
+                    verif_plat = entity.index
+                end
+            end
+        end
+    end
+
+    entities[1].onground = verif_plat ~= 0
+
+    if entities[1].onground == false then
+        components[1].yvelocity = components[1].yvelocity + 1000 * dt
+        components[1].yvelocity = math.min(components[1].yvelocity, 700)
+        components[1].coyotetimer = components[1].coyotetimer + dt
     else
-        walking = false
+        components[1].yvelocity = 0
+        components[1].coyotetimer = 0
     end
 
     if not block_xmove then
-        components[1].xpos = components[1].xpos + math.floor(components[1].xvelocity * dt + 0.5)
+        components[1].xpos = next_xpos
         components[1].xvelocity = 0
     end
     if not block_ymove then
-        components[1].ypos = components[1].ypos + math.floor(components[1].yvelocity * dt + 0.5)
+        components[1].ypos = next_ypos
     end
 end
 
@@ -245,52 +186,52 @@ local function gamestart()
 
     -- sol
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 0, ypos = 999, xsize = 1000, ysize = 10 })
     it_index = it_index + 1
 
     -- platforme
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 200, ypos = 850, xsize = 80, ysize = 20 })
     it_index = it_index + 1
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 300, ypos = 700, xsize = 80, ysize = 20 })
     it_index = it_index + 1
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 400, ypos = 550, xsize = 80, ysize = 20 })
     it_index = it_index + 1
 
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 500, ypos = 400, xsize = 80, ysize = 20 })
     it_index = it_index + 1
 
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 600, ypos = 250, xsize = 80, ysize = 20 })
     it_index = it_index + 1
 
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = false, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 700, ypos = 100, xsize = 80, ysize = 20 })
     it_index = it_index + 1
 
     -- wall
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = false, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 250, ypos = 1000 - 80, xsize = 20, ysize = 80 })
     it_index = it_index + 1
 
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = false, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 550, ypos = 500, xsize = 20, ysize = 80 })
     it_index = it_index + 1
 
     table.insert(entities,
-        { index = it_index, player = false, spawn = false, end_door = false, platforme = false, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
+        { index = it_index, player = false, spawn = false, end_door = false, platforme = true, wall = true, display = true, xpos = true, ypos = true, xsize = true, ysize = true })
     table.insert(components, { xpos = 700, ypos = 700, xsize = 20, ysize = 80 })
     it_index = it_index + 1
 
@@ -332,9 +273,7 @@ function love.update(dt)
     end
     if world.gamestate == "running" then
         inputSystem()
-        platformeCheckSystem()
-        fallingSystem(dt)
-        deplacementSystem(dt)
+        collisionPlatformDeplacementSysteme(dt)
         frameUpdate(dt)
     end
 end
